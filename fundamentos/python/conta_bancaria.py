091d5a01f008e78aca2f3185bed565128000ff98
victo <victorcesarmq.cuiaba@gmail.com>
2026-04-29 00:03:35 -0400

    mconta_bancaria.py atualizado

--- fundamentos/python/conta_bancaria.py
+++ fundamentos/python/conta_bancaria.py
@@ -1,5 +1,3 @@
-import json
-import pandas
 import datetime as dt
 
 transacoes = [] # {"Id":, "Data":, "Valor":, "Operacao":} estrutura de dicionario
@@ -9,37 +7,56 @@ saldo = 0 #variavel global
 
 def depositar(valor): # depositar valor
     global saldo
-    print(f"Depositou: R${valor}")
-    saldo += valor
-    transacoes.append({"Id": len(transacoes) + 1, "Data": dt.datetime.now().strftime("%d/%m/%Y %H:%M:%S"), "Valor": valor, "Operacao": "Deposito"})
+    if valor <= 0:
+        print("Valor Invalido")
+    else:
+        print("-" * 30)
+        print(f"Operacao finalizada com sucesso!\nDeposito realizado no valor de: R${valor}")
+        print("-" * 30)
+        saldo += valor
+        transacoes.append({"Id": len(transacoes) + 1, "Data": dt.datetime.now().strftime("%d/%m/%Y %H:%M:%S"), "Valor": valor, "Operacao": "Deposito"})
     return saldo
 
 def sacar(valor): # sacar valor
     global saldo
-    print(f"Sacou: R${valor}")
+
     if valor > saldo:
         print("Error: Saldo Indisponivel, operacao nao realizada")
         return valor
-    saldo -= valor
-    transacoes.append({"Id": len(transacoes) + 1, "Data": dt.datetime.now().strftime("%d/%m/%Y %H:%M:%S"), "Valor": valor, "Operacao": "Saque"})
+    elif valor <= 0:
+        print("Valor Invalido")
+    else:
+
+        saldo -= valor
+        print("-" * 30)
+        print(f"Operacao finalizada com sucesso!\nSaque realizado no valor de: R${valor}")
+        print("-" * 30)
+
+        transacoes.append({"Id": len(transacoes) + 1, "Data": dt.datetime.now().strftime("%d/%m/%Y %H:%M:%S"), "Valor": valor, "Operacao": "Saque"})
     return saldo
 
 def exibir_extrato(): # extrato
-    print(f"Saldo: R${saldo}\n {transacoes}")
+    for i in transacoes:
+        print("-" * 20)
+        print(f"Id:{i['Id']} \nData da transacao: {i['Data']} \nValor: R${i['Valor']:0.2f} \nOperacao realizada: {i['Operacao']}")
+        print("-" * 20)
+    print(f"Saldo: R${saldo:0.2f}")
+
 
 print("MENU")
-print("1. Sacar \n2. Depositar \n3. Extrato \n4. Sair")
+print("1. Sacar \n2. Depositar \n3. Extrato \n0. Sair")
 while True:
     try:
-        opcao = int(input("Escolha uma operacao: "))
+        opcao = int(input("Escolha uma operacao (0 para sair): "))
         if opcao == 1:
-            valor = int(input(f"Quanto desejar sacar: R$"))
+            valor = float(input(f"Quanto desejar sacar: R$"))
+
             sacar(valor)
         elif opcao == 2:
-            valor = int(input(f"Quanto deseja depositar : R$"))
+            valor = float(input(f"Quanto deseja depositar : R$"))
             depositar(valor)
         elif opcao == 3:
             exibir_extrato()
-        elif opcao == 4: break
+        elif opcao == 0: break
     except ValueError:
-        print("Digite uma opcao valida!")
+        print("Erro: valor invalido")
