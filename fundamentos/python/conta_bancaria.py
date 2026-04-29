fc40a4a228e6825ddeca4617114c2d2e048db046
victo <victorcesarmq.cuiaba@gmail.com>
2026-04-28 23:01:14 -0400

    .gitignore update conta_bancaria.py update

--- fundamentos/python/conta_bancaria.py
+++ fundamentos/python/conta_bancaria.py
@@ -1,5 +1,5 @@
 import json
-import pandas as pd
+import pandas
 import datetime as dt
 
 
@@ -17,6 +17,9 @@ def depositar(valor): # depositar valor
 def sacar(valor): # sacar valor
     global saldo
     print(f"Sacou: R${valor}")
+    if valor > saldo:
+        print("Error: Saldo Indisponivel, operacao nao realizada")
+        return valor
     saldo -= valor
     transacoes.append({"Id": len(transacoes) + 1, "Data": dt.datetime.now().strftime("%d/%m/%Y %H:%M:%S"), "Valor": valor, "Operacao": "Saque"})
     return saldo
@@ -24,6 +27,6 @@ def sacar(valor): # sacar valor
 def exibir_extrato(): # extrato
     print(f"Saldo: R${saldo}\n {transacoes}")
 
-depositar(100.6)
-sacar(1.1)
+depositar(100)
+sacar(100)
 exibir_extrato()
