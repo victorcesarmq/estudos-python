bc698bfe8a4725ce68f3acdbcffb964dfccb362e
victorcesarmq <victorcesarmq.cuiaba@gmail.com>
2026-06-04 16:07:55 -0400

    features: coletor.py esta funcionando. Todas as features necessarias por enquanto ja foram adicionadas

--- projetos/analisador-pncp/coletor.py
+++ projetos/analisador-pncp/coletor.py
@@ -4,8 +4,9 @@ import json
 import pandas as pd
 import sqlite3
 from pathlib import Path
+import time
 
-class coletor:
+class Coletor:
     def __init__(self):
 
         # Caminho padrão da pasta de dados
@@ -16,22 +17,38 @@ class coletor:
         self.dataFinal = None
         self.codigoModalidadeContratacao = None
         self.uf = None
-        self.tamanhoPagina = None
-        self.pagina = None
 
+    def verificar_cache(self):
+        return Path("dados/dados_teste.json").exists()
 
     def definir_url(self):
         url = "https://pncp.gov.br/api/consulta/v1/contratacoes/publicacao"
         return url
 
+    def solicitar_parametros(self):
+
+        print("ANO/MES/DIA")
+
+        self.dataInicial = (input("Digita a data inicial: ")
+                            .replace("-", "")
+                            .replace("/", "")
+                            .replace(" ", ""))
+
+        self.dataFinal = (input("Digita a data final: ")
+                          .replace("-", "")
+                          .replace("/", "")
+                          .replace(" ", ""))
+
+        print(self.dataInicial, self.dataFinal)
+
     def params_url(self):
         '''
-        dataInicial/FINAL = 20260101 (ANO/MES/DIA)
+        dataInicial/dataFinal = 20260101 (ANO/MES/DIA)
         '''
 
         params = {
-            "dataInicial": "20260101",
-            "dataFinal": "20260131",
+            "dataInicial": self.dataInicial,
+            "dataFinal": self.dataFinal,
             "codigoModalidadeContratacao": 6,
             "uf": "MT",
             "tamanhoPagina": 10,
@@ -39,14 +56,38 @@ class coletor:
         }
         return params
 
-    def coletar_dados(self):
-        r = requests.get(self.definir_url(), params=self.params_url(), timeout=10)
-        print(r.status_code)
-        print(r.text)
-        print(r.json())
-        if r.status_code == 200:
-            dados = r.json()
-            df_dados = pd.DataFrame(dados)
-            df_dados
-        else:
-            print(f"Erro: {r.status_code}")
+    def coletar_todas_paginas(self):
+        pagina = 1
+        todos_dados = []
+
+        while True:
+            params = self.params_url()
+            params["pagina"] = pagina
+
+            r = requests.get(
+                self.definir_url(),
+                params=params,
+                timeout=30
+            )
+            print(r.status_code)
+            print(r.text)
+            if r.status_code == 200:
+                dados = r.json()
+                todos_dados.extend(dados.get("data", []))
+                print(todos_dados)
+
+                if dados.get("paginasRestantes", 0) == 0:
+                    df_dados = pd.DataFrame(todos_dados)
+                    df_dados.to_json("./dados/dados_teste.json",
+                                     orient="records",
+                                     date_format="iso",
+                                     indent=4,
+                                     force_ascii=False
+                                     )
+                    break
+                pagina += 1
+                time.sleep(0.5)
+            else:
+                print(f"Erro: {r.status_code}")
+
+
