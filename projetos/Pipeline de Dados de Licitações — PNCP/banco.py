e3984674d18f8c83a1c7ecd8d23add084f36f2fa
victo <victorcesarmq.cuiaba@gmail.com>
2026-07-05 15:08:54 -0400

    feature: funcao para criar tabela criada porem sem uso no momento

--- projetos/Pipeline de Dados de Licitações — PNCP/banco.py
+++ projetos/Pipeline de Dados de Licitações — PNCP/banco.py
@@ -8,11 +8,27 @@ class Banco:
 
     def iniciar_banco(self):
         pass
-    def criar_tabela(self):
+
+    def criar_tabela(self): #Atualmente inutil e nao utilizada no fluxo do codigo
         self.cursor.execute(
-            '''CREATE TABLE if not exists licitacoes
-                                ''')
-        pass
+            '''CREATE TABLE if NOT EXISTS licitacoes (
+            anoCompra INTEGER NOT NULL,
+            dataInclusao TEXT NOT NULL,
+            dataPublicacaoPncp TEXT NOT NULL,
+            dataAtualizacao TEXT NOT NULL,
+            dataAberturaProposta TEXT NOT NULL,
+            dataEncerramentoProposta TEXT NOT NULL,
+            objetoCompra TEXT NOT NULL,
+            valorTotalEstimado REAL,
+            valorTotalHomologado REAL,
+            orgaoEntidade.cnpj TEXT NOT NULL,
+            orgaoEntidade.razaoSocial TEXT NOT NULL,
+            orgaoEntidade.esferaId TEXT NOT NULL,
+            situacaoCompraNome TEXT NOT NULL,
+            modalidadeNome TEXT NOT NULL,
+                                ''');
+
+
     def inserir_licitacoes(self, df):
         if utils.empty(df):
             print("Sem dados para o banco")
