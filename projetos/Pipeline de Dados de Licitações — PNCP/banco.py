715558135102aa6909147fecc49f0ece9ce1504d
victo <victorcesarmq.cuiaba@gmail.com>
2026-07-05 23:02:44 -0400

    feature: funcoes para consultas em SQL criadas

--- projetos/Pipeline de Dados de Licitações — PNCP/banco.py
+++ projetos/Pipeline de Dados de Licitações — PNCP/banco.py
@@ -25,9 +25,8 @@ class Banco:
             orgaoEntidade.razaoSocial TEXT NOT NULL,
             orgaoEntidade.esferaId TEXT NOT NULL,
             situacaoCompraNome TEXT NOT NULL,
-            modalidadeNome TEXT NOT NULL,
-                                ''');
-
+            modalidadeNome TEXT NOT NULL,);
+                                ''')
 
     def inserir_licitacoes(self, df):
         if utils.empty(df):
@@ -38,3 +37,40 @@ class Banco:
                     df[col] = df[col].astype(str)
             df.to_sql("licitacoes", self.conectar, if_exists="replace", index=False)
             self.conectar.commit()
+
+    def consultar_todas(self):
+        self.cursor.execute("SELECT * FROM licitacoes")
+        result = self.cursor.fetchall()
+        return result
+
+    def consultar_top_licitacoes(self): # Top 10 Licitacoes decrescente por valor estimado
+        self.cursor.execute(
+            '''
+            SELECT processo,"unidadeOrgao.nomeUnidade",valorTotalEstimado,valorTotalHomologado,"unidadeOrgao.municipioNome"  FROM licitacoes order by valorTotalEstimado DESC LIMIT 10 ;
+            ''')
+        result = self.cursor.fetchall()
+        return result
+
+    def consultar_top_orgaos(self): # Top 10 Órgãos decrescente por valor estimado
+        self.cursor.execute(
+            '''
+            SELECT processo,"unidadeOrgao.nomeUnidade",valorTotalEstimado,valorTotalHomologado,"unidadeOrgao.municipioNome" FROM licitacoes GROUP BY "unidadeOrgao.nomeUnidade" order by valorTotalEstimado DESC LIMIT 10 ;
+            ''')
+        result = self.cursor.fetchall()
+        return result
+
+    def consultar_por_municipio(self, municipio):
+        self.cursor.execute(
+            '''
+            SELECT "unidadeOrgao.nomeUnidade",valorTotalEstimado,valorTotalHomologado,"unidadeOrgao.municipioNome" FROM licitacoes WHERE "unidadeOrgao.municipioNome" = ?;
+            '''), (municipio,)
+        result = self.cursor.fetchall()
+        return result
+
+    def municipios_disponiveis(self):
+        self.cursor.execute(
+            '''
+            SELECT DISTINCT "unidadeOrgao.municipioNome" FROM licitacoes;
+            ''')
+        result = self.cursor.fetchall()
+        return result
