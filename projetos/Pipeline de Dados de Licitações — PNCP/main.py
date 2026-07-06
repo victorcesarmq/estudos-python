345df568a603cf26ee6489613208bcdf579e4b6b
victo <victorcesarmq.cuiaba@gmail.com>
2026-07-05 23:05:17 -0400

    apenas adicionada alguns objetos para testes. nada definitivo ainda

--- projetos/Pipeline de Dados de Licitações — PNCP/main.py
+++ projetos/Pipeline de Dados de Licitações — PNCP/main.py
@@ -1,8 +1,18 @@
 from coletor import Coletor
 from banco import Banco
+from analisador import Analisador
 
 coletor = Coletor()
 banco = Banco()
-coletor.solicitar_parametros()
-df = coletor.coletar_todas_paginas()
-banco.inserir_licitacoes(df)
+analisador = Analisador()
+
+# coletor.solicitar_parametros()
+# df = coletor.coletar_todas_paginas()
+# banco.inserir_licitacoes(df)
+# analisador.top_licitacoes(banco.consultar_top_licitacoes())
+# analisador.top_orgaos(banco.consultar_top_orgaos())
+print("Municipios disponiveis:")
+for municipios in banco.municipios_disponiveis():
+    print(municipios)
+municipio = input("Escolha um Municipio: ")
+analisador.consultar_por_municipio(banco.consultar_por_municipio(municipio))
